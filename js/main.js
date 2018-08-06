27ca01dbe29095a9df3a6f3fff2bd63bdfd4e28d
git space <gitspacee@gmail.com>
2018-08-06 17:32:14 +0100

    -

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 AOS.init();
 
+var menu = false;
+$('.mn').on('click', function() {
+    if(menu == false) {
+        $('.mn').addClass('is-active');   
+        $('.resp').slideDown(300);   
+        menu = true;  
+    } else {
+        $('.mn').removeClass('is-active');    
+        $('.resp').slideUp(300);
+        menu = false;    
+    }
+})
+
+$('.ap').on('click', function() {
+    $('.resp').fadeOut(200);
+})
+
 $(document).ready(function() {
     $('nav a[href*=\\#]').bind('click', function(e) {
       e.preventDefault(); 
@@ -13,4 +30,23 @@ $(document).ready(function() {
   
       return false;
     });
-  });
+});
+
+$(document).ready(function() {
+    $('.resp a[href*=\\#]').bind('click', function(e) {
+      e.preventDefault(); 
+      var target = $(this).attr("href"); 
+  
+      $('html, body').stop().animate({
+        scrollTop: ( $(target).offset().top - 100 ) 
+      }, 600, function() {
+        location.hash = target;
+      });
+
+      $('.mn').removeClass('is-active');    
+      $('.resp').slideUp(300);
+      menu = false; 
+  
+      return false;
+    });
+});
